f0d6e9419d43e6182484b1161e118bfd98123b17
Wiiliam_Chuang <william_chuang@asus.com>
2022-12-29 16:45:40 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <UiInput/>
+    <UiButton/>
+    <ui-button type="0"/>
+    <ui-button msg="Welcome to Your Vue.js + 1" type="1" />
+    <ui-button msg="Welcome to Your Vue.js + 2" type="2" />
   </div>
 </template>
 
-<script lang="ts">
+<!-- <script  lang="ts"> -->
+<script>
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { UiButton, UiInput } from "share_ui";
 
 @Options({
   components: {
-    HelloWorld,
+    // HelloWorld,
+    UiButton,
+    UiInput
   },
 })
 export default class Home extends Vue {}
